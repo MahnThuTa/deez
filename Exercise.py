a364b7194e7c71a71460ae034395035d906b784f
MahnThuTa <manthuta8520@gmail.com>
2022-12-11 20:21:33 +0630

    by deez

---
+++ Exercise.py
@@ -0,0 +1,12 @@
+books = 1000
+pens = 300
+manga = 2000
+comic = 2000
+if manga > pens:
+    print("Its so damn expensive")
+elif books < pens:
+    print("I aint buying this")
+elif comic == manga:
+    print("Thats kinda cool.")
+else:
+    print("Im broke.")
