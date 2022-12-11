7f78ab29d05a4129dea763066f14f38296e0f0c0
MahnThuTa <manthuta8520@gmail.com>
2022-12-11 19:47:21 +0630

    by deez

---
+++ chapter11_py101.py
@@ -0,0 +1,12 @@
+my_age = 16
+his_age = 15
+her_age = 18
+if my_age > his_age:
+    print("Im older.")
+elif her_age < my_age:
+    print("You are too old, you are gonna die.")
+elif his_age > her_age:
+    print("You guys are sanme age.")
+else:
+    print("Who asked?")
+
