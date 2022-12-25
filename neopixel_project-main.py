90d68df0fd585f3e792813cb790291264c2cfe17
MahnThuTa <manthuta8520@gmail.com>
2022-12-25 20:29:06 +0630

    project

---
+++ neopixel_project-main.py
@@ -0,0 +1,22 @@
+# Imports go at the top
+from microbit import *
+import neopixel
+
+Red = (255,0,0)
+Green = (0,255,17)
+Orange = (255,162,0)
+
+np = neopixel.NeoPixel(pin12, 4)
+
+while True:
+    np[1] = Green
+    np.show()
+    sleep(5000)
+
+    np[2] = Orange
+    np.show()
+    sleep(2000)
+
+    np[3] = Red
+    np.show()
+    sleep(5000)
